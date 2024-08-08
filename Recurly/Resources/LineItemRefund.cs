f39f5d4a3208d3965171c6896b94e2dce242e7f6
Recurly Integrations <dx@recurly.com>
2024-08-08 17:48:20 +0000

    Generated Latest Changes for v2021-02-25

--- Recurly/Resources/LineItemRefund.cs
+++ Recurly/Resources/LineItemRefund.cs
@@ -15,9 +15,17 @@ namespace Recurly.Resources
     public class LineItemRefund : Request
     {
 
+        /// <value>The specific amount to be refunded from the adjustment. Must be less than or equal to the adjustment's remaining balance. If `quantity`, `quantity_decimal` and `percentage` are not present, `amount` is required. If `quantity`, `quantity_decimal`, or `percentage` is present, `amount` must be absent.</value>
+        [JsonProperty("amount")]
+        public decimal? Amount { get; set; }
+
         /// <value>Line item ID</value>
         [JsonProperty("id")]
         public string Id { get; set; }
+
+        /// <value>The percentage of the adjustment's remaining balance to refund. If `quantity`, `quantity_decimal` and `amount_in_cents` are not present, `percentage` is required. If `quantity`, `quantity_decimal` or `amount_in_cents` is present, `percentage` must be absent.</value>
+        [JsonProperty("percentage")]
+        public int? Percentage { get; set; }
 
         /// <value>
         /// Set to `true` if the line item should be prorated; set to `false` if not.
@@ -26,11 +34,11 @@ namespace Recurly.Resources
         [JsonProperty("prorate")]
         public bool? Prorate { get; set; }
 
-        /// <value>Line item quantity to be refunded.</value>
+        /// <value>Line item quantity to be refunded. Must be less than or equal to the `quantity_remaining`. If `quantity_decimal`, `amount`, and `percentage` are not present, `quantity` is required. If `amount` or `percentage` is present, `quantity` must be absent.</value>
         [JsonProperty("quantity")]
         public int? Quantity { get; set; }
 
-        /// <value>A floating-point alternative to Quantity. If this value is present, it will be used in place of Quantity for calculations, and Quantity will be the rounded integer value of this number. This field supports up to 9 decimal places. The Decimal Quantity feature must be enabled to utilize this field.</value>
+        /// <value>Decimal quantity to refund. The `quantity_decimal` will be used to refund charges that has a NOT null quantity decimal. Must be less than or equal to the `quantity_decimal_remaining`. If `quantity`, `amount`, and `percentage` are not present, `quantity_decimal` is required. If `amount` or `percentage` is present, `quantity_decimal` must be absent. The Decimal Quantity feature must be enabled to utilize this field.</value>
         [JsonProperty("quantity_decimal")]
         public string QuantityDecimal { get; set; }
 
